f2239f5e054f18dd7d3a3e5e4f9e0bf8fa598580
Ivan Timofeev <timofeev.ivaan@gmail.com>
2024-06-06 04:55:24 +0300

    lab3.3

---
+++ Class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3._3
+{
+    class Satelite
+    {
+        public string sateliteName = " ";
+
+        public double _weight = 1;
+        public double weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value <= 0) throw new Exception("Масса спутника не может быть меньше или равна 0");
+                else _weight = value;
+            }
+        }
+
+        public double _heightOrbit = 1;
+        public double heightOrbit
+        {
+            get { return _heightOrbit; }
+            set
+            {
+                if (value < 160 || value > 36000) throw new Exception("Высота орбиты спутника не может быть меньше 160 км и больше 36000км");
+                else _heightOrbit = value;
+            }
+        }
+
+        public double _speed = 1;
+        public double speed
+        {
+            get { return _speed; }
+            set
+            {
+                if (value < 0 || value > 300000000) throw new Exception("Скорость орбиты спутника не может быть меньше нуля и больше скорости света(3*10^9 км/ч)");
+                else _speed = value;
+            }
+        }
+
+        public const int earthWidth = 40000;
+
+
+        public void printInfo ()
+        {
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"Название спутника: {this.sateliteName}\tМасса спутника: {this.weight}\tВысота орбиты: {this.heightOrbit}\tСкорость: {this.speed}\tДлина окружности Земли: {earthWidth}");
+            //Console.WriteLine("Название спутника: " + this.sateliteName + "\tМасса спутника: " + this.weight + "\tВысота орбиты: " + this.heightOrbit + "\tСкорость: " +  this.speed + "\tДлина окружности Земли: " + earthWeight);
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Satelite[] satelite = new Satelite[10];
+            int c, count = 0;
+            double weight, height, speed;
+            string name;
+            while (true)
+            {
+                count = 0;
+                foreach (var i in satelite)
+                {
+                    if (i != null)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine("Введите номер действия:\n1 - добавить\n2 - удалить\n3 - выход");
+                Console.WriteLine("count: " + count);
+                c = Convert.ToInt32(Console.ReadLine());
+                if (c == 1)
+                {
+                    if (count == 9) Console.WriteLine("К сожалению, у вас слишком много информации о спутниках, добавить новый не получится");
+                    else
+                    {
+                        Console.Write("Введите название спутника: ");
+                        name = Console.ReadLine();
+                        Console.Write("Введите массу спутника: ");
+                        weight = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Введите высоту орбиты спутника: ");
+                        height = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Введите скорость спутника: ");
+                        speed = Convert.ToDouble(Console.ReadLine());
+                        satelite[count] = new Satelite { sateliteName = name, weight = weight, heightOrbit = height, speed = speed };
+                        count++;
+                        Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
+                        foreach (var i in satelite)
+                        {
+                            if (i!=null)
+                            {
+                                Console.WriteLine($"Название спутника: {i.sateliteName}\tМасса спутника: {i.weight}\tВысота орбиты: {i.heightOrbit}\tСкорость: {i.speed}\tДлина окружности Земли: {Satelite.earthWidth}");
+                            }
+                        }
+                        Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
+                    }
+                }
+                if (c == 2)
+                {
+                    if (count == 0) Console.WriteLine("К сожалению, у вас нет информации о спутниках, удалить пустоту не получится");
+                    else
+                    {
+                        if (count == 1) satelite[0] = null;
+                        else
+                        {
+                            Console.Write("Какой спутник вы бы хотели удалить(нумерация начинается с 0)");
+                            int deleteIndex = Convert.ToInt32(Console.ReadLine());
+                            while (satelite[deleteIndex+1] != null)
+                            {
+                                satelite[deleteIndex] = satelite[deleteIndex + 1];
+                                deleteIndex++;
+                            }
+                            satelite[deleteIndex] = null;
+                            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
+                            foreach (var i in satelite)
+                            {
+                                if (i != null)
+                                {
+                                    Console.WriteLine($"Название спутника: {i.sateliteName}\tМасса спутника: {i.weight}\tВысота орбиты: {i.heightOrbit}\tСкорость: {i.speed}\tДлина окружности Земли: {Satelite.earthWidth}");
+                                }
+                            }
+                            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
+                        }
+                    }
+                }
+                if (c == 3) break;
+            }
+        }
+    }
+}
